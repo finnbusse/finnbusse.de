c81093879f69d6defb147779aedd8036ae3c8c00
Finn <finnbusse@outlook.de>
2025-01-04 00:22:15 +0100

    hopefully fixed typing animation bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,12 @@ module.exports = {
   },
   darkMode: 'class',
   plugins: [],
+
+  safelist: [
+    'from-yellow-500', 'to-pink-300',
+    'from-blue-600', 'to-blue-300',
+    'from-red-500', 'to-orange-300',
+    'from-green-500', 'to-teal-300',
+    'from-purple-500', 'to-indigo-300',
+  ],
 };
