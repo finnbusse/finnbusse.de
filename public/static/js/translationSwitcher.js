c835f89c8af87c5a2e02fe3227b02a9d58263afc
Finn <finnbusse@outlook.de>
2025-01-03 15:56:52 +0100

    changed navbar design; added translation dropdown menu

---
+++ public/static/js/translationSwitcher.js
@@ -0,0 +1,21 @@
+let translationSwitcher = function() {
+    return {
+        selected: 0,
+
+        countries: [
+            {
+                label: 'English',
+                lang: 'en',
+                flag: 'gb',
+            },
+
+            {
+                label: 'German',
+                lang: 'de',
+                flag: 'de',
+            },
+
+        ],
+        menuToggle: false,
+    }
+}
