a412fe46e2f707e17d6e2cb9549784bb153512f8
Finn <finnbusse@outlook.de>
2025-01-03 17:30:31 +0100

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "public", // Entwicklungs-Root
-    base: "./", // Relative Pfade
+    root: "public", // Der Quellordner
+    base: "/", // Basis-URL für alle Ressourcen
     build: {
-        outDir: "../dist", // Der Output-Ordner für den Build
-        emptyOutDir: true, // Löscht den Ordner vor jedem Build
-        assetsDir: "static", // Stellt sicher, dass alle Assets in "static" landen
+        outDir: "../dist", // Zielverzeichnis
+        emptyOutDir: true, // Löscht vorherige Builds
+        assetsDir: "static", // Alle Assets werden im "static"-Ordner gespeichert
         rollupOptions: {
             input: {
-                main: "./public/index.html", // Haupt-HTML
+                main: "./public/index.html", // Haupt-HTML-Datei
             },
         },
     }
